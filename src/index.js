58679ebc2128d13ba4df466633cac40e64143e92
Allyson Baffour Owusu <boallyson@gmail.com>
2023-04-19 11:50:21 -0400

    completed lab

--- src/index.js
+++ src/index.js
@@ -1,1 +1,13 @@
 // write your code here
+let ramenMenu = getElementbyid("ramen-menu")
+
+
+
+fetch("http://localhost:3000")
+.then(response => response.json())
+.then(data => rateRamen(data))
+
+function rateRamen(ramen){
+    ramen.forEach(ramen) =>
+    let image= document.createElement('img')
+}
